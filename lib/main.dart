7f4d2f117b877f2dcfd75e37ef9a6c12ad9590c4
ayushgoswami1 <ayushgoswami.abr@gmail.com>
2021-03-18 03:49:59 +0530

    Firebase Storage

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Upload Image')),
+      body: Column(
+        children: <Widget>[
+          (imageUrl != null)
+              ? Image.network(imageUrl)
+              : Placeholder(
+                  fallbackHeight: 200.0, fallbackWidth: double.infinity),
+          SizedBox(
+            height: 20.0,
+          ),
+          RaisedButton(
+            child: Text('Upload Image'),
+            color: Colors.lightBlue,
+            onPressed: () => uploadImage(),
+          )
+        ],
+      ),
+    );
+  }
+
+  uploadImage() async {
+    final _storage = FirebaseStorage.instance;
+    final _picker = ImagePicker();
+    PickedFile image;
+    await Permission.photos.request();
+
+    var permissionStatus = await Permission.photos.status;
+
+    if (permissionStatus.isGranted) {
+      image = await _picker.getImage(source: ImageSource.gallery);
+      var file = File(image.path);
+
+      if (image != null) {
+        var snapshot = await _storage
+            .ref()
+            .child('folderName/imageName')
+            .putFile(file)
+            .onComplete;
+
+        var downloadUrl = await snapshot.ref.getDownloadURL();
+
+        setState(() {
+          imageUrl = downloadUrl;
+        });
+      } else {
+        print('No Path Received');
+      }
+    } else {
+      print('Grant Permissions and try again');
+    }
+  }
+}
